e814a311e4ccb77b9c5fb496c3d2e9c88eee8c24
wangyiyuelva <yiyuwang891120@gmail.com>
2024-04-13 16:58:07 +0000

    add rabbitmq basic producer/consumer

---
+++ rabbitmq/producer.py
@@ -0,0 +1,15 @@
+import pika
+
+def send(input):
+input = "../data_input/test.mp4"
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost', '5673'))
+channel = connection.channel()
+
+channel.queue_declare(queue='input_file_que')
+		
+		
+channel.basic_publish(exchange='',
+                          routing_key='input_file_que',
+                          body=input)
+print(" [x] Sent {input}")
+channel.close()
